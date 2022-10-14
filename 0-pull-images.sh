f974a309cd9d14328ed67b7c0cd27fa155a54715
sorriso <sorriso.tls@gmail.com>
2022-10-14 18:43:37 +0200

    #1

---
+++ 0-pull-images.sh
@@ -0,0 +1,2 @@
+nerdctl -n k8s.io pull jenkins/jenkins:2.362-rhel-ubi8-jdk11
+nerdctl -n k8s.io pull jenkins/jenkins:2.361.1-lts-jdk11
---
+++ 1-start-nginx.sh
@@ -0,0 +1,2 @@
+nerdctl -n k8s.io pull nginx:1.23.1-alpine
+kubectl apply -f nginx
